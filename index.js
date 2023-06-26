5dd0c9818b100c07405fb4e86787a790193eea3e
EmelyanovI <dev.emelyanovii@gmai.com>
2023-06-26 22:35:56 +0500

    v2.25.06.23

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let post = {
-    time: '',
-    title: '',
-    text: '',
-};
+const posts = [];
+
+//Объявление переменных - Строковых констант
+const TITLE_VALIDATION_LIMIT = 10;
+const TEXT_VALIDATION_LIMIT = 30;
 
 //Объвление переменных - ссылок на HTML элементы
-const postTitleInputNode = document.getElementById('post__header')
+const postTitleInputNode = document.getElementById('post__header');
 const postTextInputNode = document.getElementById('post__text');
 const newPostBtnNode = document.getElementById('new-post__btn')
 const postsNode = document.getElementById('feed__posts');
+
+const validationMessageHeaderNode = document.getElementById('validation-post-header');
+const validationMessageTextNode = document.getElementById('validation-post-text');
+
 
 //Привязка функции-обработчика к кнопкам
 //привязка к кнопке опубликовать
@@ -16,36 +20,97 @@ newPostBtnNode.addEventListener('click', () => {
     //получаем данные из поля ввода
     const postFromUser = getPostFromUser();
     //присвоение значения из поля ввода
-    setPost(postFromUser);
+    addPost(postFromUser);
     //отображение поста
-    renderPost();
+    renderPosts();
+    clearInput();//очистка полей ввода
+});
+
+//Проверка на количество символов в поле ввода
+postTitleInputNode.addEventListener('input', (event) => {
+    const currentValue = event.target.value;
+
+    if (currentValue.length >= TITLE_VALIDATION_LIMIT) {
+        validationMessageHeaderNode.innerHTML = `
+        <div class="validation-post-header__max">
+        Достигнут лимит в ${TITLE_VALIDATION_LIMIT} символов:(</div>`;
+        validationMessageHeaderNode.classList.remove('validation-post-header__hidden');
+    } else {
+        validationMessageHeaderNode.classList.add('validation-post-header__hidden');
+    };
+});
+postTextInputNode.addEventListener('input', (event) => {
+    const currentValue = event.target.value;
+
+    if (currentValue.length >= TEXT_VALIDATION_LIMIT) {
+        validationMessageTextNode.innerHTML = `
+        <div class="validation-post-text__max">
+        Достигнут лимит в ${TEXT_VALIDATION_LIMIT} символов:(</div>`;
+        validationMessageTextNode.classList.remove('validation-post-text__hidden');
+    } else {
+        validationMessageTextNode.classList.add('validation-post-text__hidden');
+    };
 });
 
 //получыем данные из поля ввода
 getPostFromUser = () => {
     const title = postTitleInputNode.value
+
+    if ( !title || title.length < 5) {
+        alert('Заголовок не менее 5 символов');   
+    };
+
     const text = postTextInputNode.value
 
+    if ( !text || text.length < 10) {
+        alert('Текст поста не менее 10 символов');   
+    }
     return { //возвращаем объект состоящией из титла и текста
         title: title,
         text: text,
     };
 };
-//сохраняем заголовок поста
-setPost = (newPost) => {
-    post = newPost;
+const clearInput = () => {
+    postTitleInputNode.value = '';
+    postTextInputNode.value = '';
 };
-getPost = () => {
-    return post;
+//добавление поста в массив
+addPost = ({title, text}) => {
+    const currentDate = new Date();
+
+    const createdDate = currentDate
+    .toLocaleDateString("ru-RU", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    })
+    .replace(/ г\.$/, "");
+    const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+    const createdTime = `${currentDate.getHours()}:${minutes}`;
+    const createdDateTime = `${createdDate} ${createdTime}`;
+
+    posts.push({
+        title: title,
+        text: text,
+        created: createdDateTime
+    });
 };
-//отобразить постa
-renderPost = () => {
-    const post = getPost();
+//получаем пост
+getPosts = () => {
+    return posts;
+};
+//вывод постa
+renderPosts = () => {
+    const posts = getPosts();
 
-    console.log(getPost());
-    const postHTML = `
-    <h3 class="feed__title text-title-style">${post.title}</h3>
-    <p class="feed__text text-style">${post.text}</p>`;
+    let postsHTML = '';
 
-    postsNode.innerHTML = postHTML;
+    posts.forEach(post => {
+        postsHTML += `
+        <time class="feed__time">${post.created}</time>
+        <h3 class="feed__title">${post.title}</h3>
+        <p class="feed__text">${post.text}</p>`
+    });
+
+    postsNode.innerHTML = postsHTML;
 };
