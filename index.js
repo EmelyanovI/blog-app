466d0678be29e6263748c4ba08c4d9f6a3b93d3b
EmelyanovI <dev.emelyanovii@gmai.com>
2023-06-30 22:31:30 +0500

    v.30.06.2023

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const posts = [];
 
 //Объявление переменных - Строковых констант
 const TITLE_VALIDATION_LIMIT = 10;
-const TEXT_VALIDATION_LIMIT = 30;
+const TITLE_VALIDATION_MIN_LIMIT = 5;
+const TEXT_VALIDATION_LIMIT = 20;
 
 //Объвление переменных - ссылок на HTML элементы
 const postTitleInputNode = document.getElementById('post__header');
 const postTextInputNode = document.getElementById('post__text');
 const newPostBtnNode = document.getElementById('new-post__btn')
 const postsNode = document.getElementById('feed__posts');
+const resultHeaderNode = document.getElementById('result__header');
+const resultTextNode = document.getElementById('result__text');
 
 const validationMessageHeaderNode = document.getElementById('validation-post-header');
 const validationMessageTextNode = document.getElementById('validation-post-text');
@@ -27,30 +30,37 @@ newPostBtnNode.addEventListener('click', () => {
 });
 
 //Проверка на количество символов в поле ввода
-postTitleInputNode.addEventListener('input', (event) => {
-    const currentValue = event.target.value;
-
-    if (currentValue.length >= TITLE_VALIDATION_LIMIT) {
-        validationMessageHeaderNode.innerHTML = `
-        <div class="validation-post-header__max">
-        Достигнут лимит в ${TITLE_VALIDATION_LIMIT} символов:(</div>`;
-        validationMessageHeaderNode.classList.remove('validation-post-header__hidden');
-    } else {
-        validationMessageHeaderNode.classList.add('validation-post-header__hidden');
-    };
+postTitleInputNode.addEventListener('input', () => {
+    validationTitle();
 });
-postTextInputNode.addEventListener('input', (event) => {
-    const currentValue = event.target.value;
-
-    if (currentValue.length >= TEXT_VALIDATION_LIMIT) {
-        validationMessageTextNode.innerHTML = `
-        <div class="validation-post-text__max">
-        Достигнут лимит в ${TEXT_VALIDATION_LIMIT} символов:(</div>`;
-        validationMessageTextNode.classList.remove('validation-post-text__hidden');
-    } else {
-        validationMessageTextNode.classList.add('validation-post-text__hidden');
-    };
+postTextInputNode.addEventListener('input', () => {
+    validationText();
 });
+
+validationTitle = () => {
+    const titleLen = postTitleInputNode.value.length;
+
+    //визуализация колличества символов
+    validationMessageHeaderNode.innerText = `${titleLen}/${TITLE_VALIDATION_LIMIT}`;
+
+    if (titleLen >= TITLE_VALIDATION_LIMIT) {
+        validationMessageHeaderNode.innerHTML = `<div class="red__text" >
+        Достигнут лимит символов ${titleLen}/${TITLE_VALIDATION_LIMIT}</div>`;
+        return;
+    };
+};
+validationText = () => {
+    const textLen = postTextInputNode.value.length;
+    
+    //визуализация колличества символов
+    validationMessageTextNode.innerText = `${textLen}/${TEXT_VALIDATION_LIMIT}`;
+
+    if (textLen > TEXT_VALIDATION_LIMIT) {
+        validationMessageTextNode.innerHTML = `<div class="red__text" >
+        Лимит символов превышен ${textLen}/${TEXT_VALIDATION_LIMIT}</div>`;
+        return;
+    };
+};
 
 //получыем данные из поля ввода
 getPostFromUser = () => {
