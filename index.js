f63965ba699b9504a2ebe3cc99adccfa797ee959
EmelyanovI <dev.emelyanovii@gmai.com>
2023-07-02 16:26:16 +0500

    v.02.07.23

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const posts = [];
 
 //Объявление переменных - Строковых констант
-const TITLE_VALIDATION_LIMIT = 10;
-const TITLE_VALIDATION_MIN_LIMIT = 5;
-const TEXT_VALIDATION_LIMIT = 20;
+const TITLE_VALIDATION_LIMIT = 30;
+const TEXT_VALIDATION_LIMIT = 500;
+const VALIDATION_MIN_LIMIT = 4;
 
 //Объвление переменных - ссылок на HTML элементы
 const postTitleInputNode = document.getElementById('post__header');
@@ -16,17 +16,14 @@ const resultTextNode = document.getElementById('result__text');
 const validationMessageHeaderNode = document.getElementById('validation-post-header');
 const validationMessageTextNode = document.getElementById('validation-post-text');
 
-
 //Привязка функции-обработчика к кнопкам
 //привязка к кнопке опубликовать
 newPostBtnNode.addEventListener('click', () => {
-    //получаем данные из поля ввода
-    const postFromUser = getPostFromUser();
-    //присвоение значения из поля ввода
-    addPost(postFromUser);
-    //отображение поста
-    renderPosts();
+    const postFromUser = getPostFromUser(); //получаем данные из поля ввода
+    addPost(postFromUser);//присвоение значения из поля ввода
+    renderPosts(); //отображение поста
     clearInput();//очистка полей ввода
+    clearValidation();//очистка количества символов
 });
 
 //Проверка на количество символов в поле ввода
@@ -39,50 +36,73 @@ postTextInputNode.addEventListener('input', () => {
 
 validationTitle = () => {
     const titleLen = postTitleInputNode.value.length;
-
     //визуализация колличества символов
     validationMessageHeaderNode.innerText = `${titleLen}/${TITLE_VALIDATION_LIMIT}`;
 
-    if (titleLen >= TITLE_VALIDATION_LIMIT) {
-        validationMessageHeaderNode.innerHTML = `<div class="red__text" >
-        Достигнут лимит символов ${titleLen}/${TITLE_VALIDATION_LIMIT}</div>`;
-        return;
+    if (titleLen != 0 || titleLen > VALIDATION_MIN_LIMIT) {
+        if (titleLen <= VALIDATION_MIN_LIMIT) {
+            validationMessageHeaderNode.innerHTML = `<div class="red__text" >
+            Нужно минимум ${VALIDATION_MIN_LIMIT + 1} символов:(</div>`;
+        } else {
+        };
+        if (titleLen > TITLE_VALIDATION_LIMIT) {
+            validationMessageHeaderNode.innerHTML = `<div class="red__text" >
+            Лимит символов превышен на -${titleLen - TITLE_VALIDATION_LIMIT}</div>`;
+            return;
+        };
+    } else {
+        clearValidation();
     };
+
 };
 validationText = () => {
     const textLen = postTextInputNode.value.length;
     
     //визуализация колличества символов
-    validationMessageTextNode.innerText = `${textLen}/${TEXT_VALIDATION_LIMIT}`;
+    validationMessageTextNode.innerHTML = `${textLen}/${TEXT_VALIDATION_LIMIT}`;
 
-    if (textLen > TEXT_VALIDATION_LIMIT) {
-        validationMessageTextNode.innerHTML = `<div class="red__text" >
-        Лимит символов превышен ${textLen}/${TEXT_VALIDATION_LIMIT}</div>`;
-        return;
+    if (textLen != 0 || textLen > VALIDATION_MIN_LIMIT) {
+        if (textLen <= VALIDATION_MIN_LIMIT) {
+            validationMessageTextNode.innerHTML = `<div class="red__text" >
+            Нужно минимум ${VALIDATION_MIN_LIMIT + 1} символов:(</div>`;
+            newPostBtnNode.disabled = true;
+            return;
+        }
+        if (textLen > TEXT_VALIDATION_LIMIT) {
+            validationMessageTextNode.innerHTML = `<div class="red__text" >
+            Лимит символов превышен ${textLen}/${TEXT_VALIDATION_LIMIT}</div>`;
+            newPostBtnNode.disabled = true;
+            return;
+        } else {
+            newPostBtnNode.disabled = false;
+        };
+    } else {
+        clearValidation();
     };
 };
 
 //получыем данные из поля ввода
-getPostFromUser = () => {
+const getPostFromUser = () => {
     const title = postTitleInputNode.value
-
-    if ( !title || title.length < 5) {
-        alert('Заголовок не менее 5 символов');   
-    };
-
     const text = postTextInputNode.value
 
-    if ( !text || text.length < 10) {
-        alert('Текст поста не менее 10 символов');   
-    }
+    //проверка на минимум символов
+    if (!title || !text || title.length <= VALIDATION_MIN_LIMIT || text.length <= VALIDATION_MIN_LIMIT) {
+        alert(`Заголовок и текст должны быть длинее ${VALIDATION_MIN_LIMIT + 1} символов`);
+        return;
+      }
     return { //возвращаем объект состоящией из титла и текста
         title: title,
         text: text,
     };
 };
-const clearInput = () => {
+const clearInput = () => {//чистим поле ввода
     postTitleInputNode.value = '';
     postTextInputNode.value = '';
+};
+const clearValidation = () =>{//чистим счетчик символов
+    validationMessageHeaderNode.innerText = '';
+    validationMessageTextNode.innerText = '';
 };
 //добавление поста в массив
 addPost = ({title, text}) => {
